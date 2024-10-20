5b276bb9a395e06f25184b64e112e33a8bdbea74
rayenne brik <rayenne_brik@live.fr>
2024-10-20 13:43:31 +0200

    add configuration server and file

--- config-service/src/main/java/dev/rayenne/ConfigService.java
+++ config-service/src/main/java/dev/rayenne/ConfigService.java
@@ -1,13 +1,16 @@
 package dev.rayenne;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class ExamServiceApplication {
+public class ConfigService {
     public static void main(String[] args) {
-        SpringApplication.run(ExamServiceApplication.class, args);
+        SpringApplication.run(ConfigService.class);
     }
 }
