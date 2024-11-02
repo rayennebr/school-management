0fde249c780234d99771fba107048af45fb1b1e7
rayenne brik <rayenne_brik@live.fr>
2024-11-02 11:14:30 +0100

    add api gateway

---
+++ api-gateway/src/main/java/dev/rayenne/ApiGateway.java
@@ -0,0 +1,12 @@
+package dev.rayenne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class ApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateway.class);
+    }
+}
